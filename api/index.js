a337bb9e6e6f142177215e2b166ca02ed057dc2c
BrayanMen <brayanjmr880@gmail.com>
2023-06-18 01:45:28 -0300

    PORT

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const server = require('./app')
-const PORT = 3000;
+const PORT = 80 || 3000;
 
 server.listen(PORT, () => {
         console.log((`El servidor esta funcionando en el puerto ${PORT}`));
